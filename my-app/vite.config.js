f21ec24941fced02d10b96ccb39f7e6737430e6f
Sanjeev <rauniyarsanjeev742@gmail.com>
2025-11-07 18:36:17 +0545

    Updated vercel and vite

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react' 
 
 export default defineConfig({
-  plugins: [
-    react(),      
-    tailwindcss(),
-  ],
-  base: './',     
+  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist',
+  },
 })
